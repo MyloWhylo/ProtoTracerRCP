3a3901358a564da877e02d96f825a3dac36efe1c
Owen Vogelgesang <ovogelge@iu.edu>
2022-07-10 17:20:09 -0400

    Fixed gitignore (maybe)

--- build/src/CMakeFiles/displayDriver_ws2812_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../../src/ws2812.pio.h"
-  "CMakeFiles/displayDriver_ws2812_pio_h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/displayDriver_ws2812_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
